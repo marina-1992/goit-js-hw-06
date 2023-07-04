ea927f4e392f4fb482ac1fbf6545bc9011e0fff6
marina-1992 <mariwkabeauty@gmail.com>
2023-07-04 13:36:08 +0300

    version-2 mistacs in task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,10 +1,20 @@
 
 const loginForm = document.querySelector('.login-form');
-loginForm.addEventListener('submit', (event) => {
+loginForm.addEventListener('submit', submitEvent);
+function submitEvent(event) {
   event.preventDefault()
+    
   const { elements: { email, password } } = event.currentTarget;
+    
   if (email.value === "" || password.value === "") {
-    alert('All fields must be filled in')
-  } console.log(`Login : ${email.value}, Password : ${password.value}`);
-  event.currentTarget.reset()
-});
+    alert('All fields must be filled in');
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    
+    console.log(formData);
+    event.currentTarget.reset()
+  }
+};
